1407a69318d621f2ca0dabc0120b6d484b89acc2
Daniel <dhatton@ycp.edu>
2014-03-19 20:31:45 -0400

    Minor Updat to Classes

--- MartinDatabase/src/edu/ycp/cs320/db/IDatabase.java
+++ MartinDatabase/src/edu/ycp/cs320/db/IDatabase.java
@@ -1,6 +1,8 @@
 package edu.ycp.cs320.db;
 import java.sql.SQLException;
 
+import edu.ycp.cs320.db.model.MemberData;
+
 public interface IDatabase {
 
 	public MemberData retrieveData(RequestHandler request) throws SQLException;
--- MartinDatabase/src/edu/ycp/cs320/db/RequestHandler.java
+++ MartinDatabase/src/edu/ycp/cs320/db/RequestHandler.java
@@ -1,5 +1,9 @@
 package edu.ycp.cs320.db;
 
+import java.util.List;
+import java.util.Scanner;
+
+
 public class RequestHandler {
 	
 }
