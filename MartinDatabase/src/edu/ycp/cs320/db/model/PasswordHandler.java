9d9db760b3d4d114a1ca5e83f959bbbd660ef87d
Daniel <dhatton@ycp.edu>
2014-03-21 11:25:21 -0400

    Moved Passwordhandler, since it is a model item

---
+++ MartinDatabase/src/edu/ycp/cs320/db/model/PasswordHandler.java
@@ -0,0 +1,30 @@
+package edu.ycp.cs320.db.model;
+
+import edu.ycp.cs320.db.IDatabase;
+
+
+public class PasswordHandler {
+
+	public PasswordHandler() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	private boolean verifyPass(String param, IDatabase db){
+		String decrpyt;
+		
+		//TODO: Figure out Encryption for incoming data as well as sifting through unsafe data
+		// Perhaps some sort of Class that handles that? 
+		
+		if(){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+		
+
+}
