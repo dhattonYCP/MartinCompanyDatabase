8b542b572243050b8b11edfe21a5359e77325906
Daniel <dhatton@ycp.edu>
2014-03-21 11:36:04 -0400

    Verifying Commit

--- MartinDatabase/src/edu/ycp/cs320/db/model/PasswordHandler.java
+++ MartinDatabase/src/edu/ycp/cs320/db/model/PasswordHandler.java
@@ -13,18 +13,10 @@ public class PasswordHandler {
 	 * @param args
 	 */
 	private boolean verifyPass(String param, IDatabase db){
-		String decrpyt;
-		
+		String decrypt = null;
 		//TODO: Figure out Encryption for incoming data as well as sifting through unsafe data
 		// Perhaps some sort of Class that handles that? 
-		
-		if(){
 			return true;
-		}
-		else{
-			return false;
-		}
-	}
-		
+	}	
 
 }
