a9734b83141872d97dd3102bbad2e3d0322ea3a0
Chad Bechtold <canadiangodofwar@gmail.com>
2014-04-07 11:19:06 -0400

    initial version of GWT GUI

---
+++ MartinWeb/src/martinComp/FMC/IFMA/model/User.java
@@ -0,0 +1,29 @@
+package martinComp.FMC.IFMA.model;
+public class User {
+		private String password;
+		private String user;
+		private boolean alertNotification;
+		
+		public void setUserName(String user){
+			this.user = user;
+		}
+
+		public String getUserName() {
+			return user;
+		}
+
+		public String getPassword() {
+			return password;
+		}
+
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		public void setAlertNotification(boolean alertNotification){
+			this.alertNotification = alertNotification;
+		}
+		public boolean getAlertNotification(){
+			return alertNotification;
+		}
+}
+
